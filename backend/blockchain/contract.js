16985b039dfcd3158d7732bd9b30c2a6a416487b
ZahraNrzz <zahra.nrz.z4@gmail.com>
2026-07-21 17:21:35 +0330

    adding project

--- backend/blockchain/contract.js
+++ backend/blockchain/contract.js
@@ -4,14 +4,14 @@ const provider = new ethers.JsonRpcProvider(
     "http://127.0.0.1:8545"
 );
 
-const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const privateKey = "your_private_key";
 
 const wallet = new ethers.Wallet(
     privateKey,
     provider
 );
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "your_contract_address";
 
 const contractJson = require("./DatasetRequest.json");
 
